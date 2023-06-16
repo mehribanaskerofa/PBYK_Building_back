82e495a14798de8e663b2cfd4ad08fbfebb3e3f6
mehribanaskerofa <m.askerova.99@gmail.com>
2023-06-16 09:20:21 +0400

    init

---
+++ resources/views/admin/includes/menu.blade.php
@@ -0,0 +1,8 @@
+<li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">Layout</div> <i class="icon-menu" title="Layout options"></i></li>
+<li class="nav-item nav-item-submenu">
+    <a href="#" class="nav-link"><i class="icon-stack2"></i> <span>Page layouts</span></a>
+
+    <ul class="nav nav-group-sub" data-submenu-title="Page layouts">
+        <li class="nav-item"><a href="layout_static.html" class="nav-link">Static layout</a></li>
+    </ul>
+</li>
---
+++ routes/admin-route.php
@@ -0,0 +1,29 @@
+<?php
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\ProductImageController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/admin/login',[AdminController::class,'loginView'])->name('admin.login-view');
+Route::post('/admin/login',[AdminController::class,'login'])->name('admin.login');
+
+function productRoutes():void{
+    Route::resource('product',ProductController::class);
+
+    Route::resource('product-image',ProductImageController::class)->except(['index','create','show']);
+    Route::get('product-image/{productId}',[ProductImageController::class,'index'])->name('product-image.index');
+    Route::get('product-image/create/{productId}',[ProductImageController::class,'create'])->name('product-image.create');
+    Route::post('sort-product-image',[ProductImageController::class,'sort'])->name('product-image-sort');
+}
+
+
+//,'middleware'=>'admin'
+Route::group(['prefix'=>'admin','as'=>'admin.'],function () {
+    Route::get('/',[AdminController::class,'index'])->name('home');
+    Route::get('/logout',[AdminController::class,'logout'])->name('logout');
+
+
+
+    productRoutes();
+
+});
